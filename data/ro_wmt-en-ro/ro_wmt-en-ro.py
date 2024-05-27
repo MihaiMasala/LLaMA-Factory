793f938c7667085006478b35d91241760e969f57
Mihai Masala <mihaimasala@gmail.com>
2024-05-27 14:01:42 +0300

    add ro_wmt

---
+++ data/ro_wmt-en-ro/ro_wmt-en-ro.py
@@ -0,0 +1,33 @@
+import json
+import os
+from typing import List
+import sys
+import datasets
+
+description = "Tradu următorul text din engleză în română: "
+
+class WMTRoEN(datasets.GeneratorBasedBuilder):
+    VERSION = datasets.Version("0.0.0")
+
+    def _info(self):
+        features = datasets.Features(
+            {"prompt": datasets.Value("string"),
+             "response": datasets.Value("string")}
+        )
+        return datasets.DatasetInfo(features=features)
+
+    def _split_generators(self, dl_manager):
+        file_path = dl_manager.download("https://huggingface.co/datasets/wmt/wmt16/resolve/main/ro-en/train-00000-of-00001.parquet")
+        return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": file_path})]
+
+    def _generate_examples(self, filepath: str):
+        dataset = datasets.load_dataset("parquet", data_files=filepath)
+        entry_id = -1
+        for data_entry in dataset["train"]:
+            entry_id += 1
+            yield entry_id, {
+                "prompt": "{0}{1}\n".format(description, data_entry["translation"]["en"]),
+                "response": data_entry["translation"]["ro"]
+            }
+
+        
