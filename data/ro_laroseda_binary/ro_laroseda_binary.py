18a2fa90b9c797c5a54eecef986a30aeefa88db9
Mihai Masala <mihaimasala@gmail.com>
2024-05-29 15:06:52 +0300

    Add dot between title and content

--- data/ro_laroseda_binary/ro_laroseda_binary.py
+++ data/ro_laroseda_binary/ro_laroseda_binary.py
@@ -24,7 +24,7 @@ class LarosedaMultiClass(datasets.GeneratorBasedBuilder):
             data_list = json.load(f)["reviews"]
             for entry_id, entry in enumerate(data_list):
                 yield entry_id, {
-                    "prompt": "{2}Recenzie: {0} {1}\nEvaluare:".format(entry["title"].strip(), entry["content"].strip(), description),
+                    "prompt": "{2}Recenzie: {0}. {1}\nEvaluare:".format(entry["title"].strip(), entry["content"].strip(), description),
                     "response": "pozitivă" if int(entry["starRating"]) > 3 else "negativă"
                 }
 
